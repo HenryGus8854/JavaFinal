7917408dc51fbf3012a6aa3896751f65fea80e9a
Eduardo Amorim <63356025+Edu-Amorim@users.noreply.github.com>
2022-12-17 23:37:17 -0400

    Quiz Results and filtering works

--- web/quizResults.js
+++ web/quizResults.js
@@ -2,6 +2,9 @@ let allQuizResults = [];
 
 window.onload = function () {
     getAllQuizResults();
+    document.querySelector("#refresh").addEventListener("click", getAllQuizResults);
+    document.querySelector("#searchByTagButton").addEventListener("click", filterTableByTitle);
+
 };
 
 function getAllQuizResults() {
@@ -14,9 +17,10 @@ function getAllQuizResults() {
             if (resp.search("ERROR") >= 0) {
                 alert("oh no, something is wrong with getting quiz results");
             } else {
-                console.log(resp);
                 allQuizResults = JSON.parse(resp);
                 buildTableBody(JSON.parse(resp));
+                // console.log(resp);
+                // console.log(allQuizResults);
             }
         }
     };
@@ -29,3 +33,27 @@ function clearTable(){
     theTable.innerHTML = theTable.querySelector("tr").innerHTML;
 }
 
+function buildTableBody(allQuizResults) {
+    clearTable();
+    let theTable = document.querySelector("table");
+    let html = theTable.querySelector("tr").innerHTML;
+
+    for (let result of allQuizResults) {
+        html += `<tr id="${result.resultID}">`;
+        html += `<td>${result.resultID}</td>`;
+        html += `<td>${result.user.username}</td>`;
+        html += `<td>${result.quiz.quizTitle}</td>`;
+        html += `<td>${Math.floor((result.scoreNumerator / result.scoreDenominator) * 100)}</td>`;
+        html += `</tr>`;
+    }
+    //console.log(theTable);
+    theTable.innerHTML = html;
+}
+
+function filterTableByTitle() {
+    let userTitleInput = document.querySelector("#userTitleInput").value;
+    let regex =  new RegExp(userTitleInput);
+    let filteredArray = allQuizResults.filter( result => regex.test(result.user.username));
+    buildTableBody(filteredArray);
+    document.querySelector("#userTitleInput").value = "";
+}
